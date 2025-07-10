e13d984de75fc8901036584326344326e09c55e7
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-10 18:08:32 +0530

    Added axios interceptors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,13 @@ import "../stylesheets/application.scss";
 import ReactRailsUJS from "react_ujs";
 import App from "../src/App";
 
-import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
+import initializeAxios from "../src/apis/axios";
 
 initializeLogger();
-setAuthHeaders();
+initializeAxios();
 
 const componentsContext = { App };
-ReactRailsUJS.getConstructor = (name) => {
+ReactRailsUJS.getConstructor = name => {
   return componentsContext[name];
 };
--- app/javascript/src/apis/axios.js
+++ app/javascript/src/apis/axios.js
@@ -1,10 +1,31 @@
+import {
+  keysToCamelCase,
+  serializeKeysToSnakeCase,
+} from "@bigbinary/neeto-cist";
 import axios from "axios";
+import { evolve } from "ramda";
 
 import { AXIOS_BASE_URL } from "../constants";
 
-axios.defaults.baseURL = AXIOS_BASE_URL;
+const transformKeysToCamelCase = response => {
+  if (response.data) response.data = keysToCamelCase(response.data);
+};
 
-export const setAuthHeaders = () => {
+const responseInterceptors = () => {
+  axios.interceptors.response.use(response => {
+    transformKeysToCamelCase(response);
+
+    return response.data;
+  });
+};
+
+const requestInterceptors = () => {
+  axios.interceptors.request.use(
+    evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
+  );
+};
+
+const setHttpHeaders = () => {
   axios.defaults.headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -13,3 +34,12 @@ export const setAuthHeaders = () => {
       .getAttribute("content"),
   };
 };
+
+const initializeAxios = () => {
+  axios.defaults.baseURL = AXIOS_BASE_URL;
+  setHttpHeaders();
+  responseInterceptors();
+  requestInterceptors();
+};
+
+export default initializeAxios;
