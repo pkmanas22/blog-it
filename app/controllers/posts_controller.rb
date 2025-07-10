5b36e2fde24d8d13c35751e2ea67c3c0cea85be1
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-10 14:49:39 +0530

    Created posts route for fetching all the blog posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+  def index
+    posts = Post.all
+
+    render status: :ok, json: { posts: }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "home#index"
+  resources :posts, only: :index
 
+  root "home#index"
   get "*path", to: "home#index", via: :all
 end
