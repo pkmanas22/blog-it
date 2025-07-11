1094514421fddfcab0a7d72b7c3db5f3d591cfc2
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-11 11:50:56 +0530

    Added slug based routing in backend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,14 @@ class PostsController < ApplicationController
 
     render status: :ok, json: { posts: }
   end
+
+  def show
+    @post = Post.find_by(slug: params[:slug])
+
+    if @post
+      render status: :ok, json: { post: @post }
+    else
+      render status: :not_found, json: { error: "Post not found" }
+    end
+  end
 end
