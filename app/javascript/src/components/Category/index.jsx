0f98b78dd1693fddb5b3f6c3144d0b227bf2d0e1
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-16 02:43:36 +0530

    Added all blogs to category page

--- app/javascript/src/components/Category/index.jsx
+++ app/javascript/src/components/Category/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import { PageHeader } from "components/common";
+import { Button } from "neetoui";
+import { Link } from "react-router-dom";
+import routes from "routes";
+import withT from "utils/withT";
+
+import CategoryWiseList from "./List";
 import CategoriesSidebar from "./Sidebar";
 
-const Category = () => <CategoriesSidebar />;
+const Category = ({ t }) => (
+  <div className="flex">
+    <CategoriesSidebar />
+    <div className="h-screen flex-1 py-8 pl-8">
+      <PageHeader label={t("blogPosts")}>
+        <Link to={routes.blogs.create}>
+          <Button label={t("addNewBlog")} />
+        </Link>
+      </PageHeader>
+      <div className="h-full overflow-y-auto p-6">
+        <CategoryWiseList />
+      </div>
+    </div>
+  </div>
+);
 
-export default Category;
+export default withT(Category);
--- app/javascript/src/components/common/index.js
+++ app/javascript/src/components/common/index.js
@@ -1,6 +1,7 @@
+import BlogCard from "./Card";
 import PageHeader from "./PageHeader";
 import PageLoader from "./PageLoader";
 import PageNotFound from "./PageNotFound";
 import Sidebar from "./Sidebar";
 
-export { Sidebar, PageHeader, PageLoader, PageNotFound };
+export { Sidebar, PageHeader, PageLoader, PageNotFound, BlogCard };
