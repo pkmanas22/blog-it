41211422c7fa2e1037dd8634d88f7bd872bbaad7
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-20 00:18:24 +0530

    Updated validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,35 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  MINIMUM_PASSWORD_LENGTH = 6
-  MAXIMUM_PASSWORD_LENGTH = 16
+  MAX_NAME_LENGTH = 35
+  MIN_PASSWORD_LENGTH = 6
+  MAX_EMAIL_LENGTH = 255
 
   belongs_to :organization
   has_many :posts
 
   has_secure_password
 
-  validates :name, presence: true
+  validates :name,
+    presence: true,
+    length: { maximum: MAX_NAME_LENGTH }
   validates :email,
     presence: true,
-    uniqueness: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: MAX_EMAIL_LENGTH },
     format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password,
     presence: true,
     length: {
-      minimum: MINIMUM_PASSWORD_LENGTH,
-      maximum: MAXIMUM_PASSWORD_LENGTH
-    },
-    confirmation: true, if: -> { new_record? || !password.nil? }
+      minimum: MIN_PASSWORD_LENGTH
+    }, if: -> { password.present? }
+  validates :password_confirmation, presence: true, on: :create
+
+  before_save :to_lowercase
+
+  private
+
+    def to_lowercase
+      email.downcase!
+    end
 end
