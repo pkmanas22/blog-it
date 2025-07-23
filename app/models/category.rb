6011e4b518e930acb5adf9064e12358690fef588
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-23 14:40:43 +0530

    Fixed category taken error

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,7 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :posts
   belongs_to :organization
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false, scope: :organization_id }
 
   validates :organization_id, presence: true
 end
