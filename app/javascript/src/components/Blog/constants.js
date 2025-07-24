291642a31cca65c583750b5d741d13e9a451eb7e
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-24 17:53:34 +0530

    Updated validation for maximum length of title & description

--- app/javascript/src/components/Blog/constants.js
+++ app/javascript/src/components/Blog/constants.js
@@ -16,12 +16,16 @@ export const POST_FORM_VALIDATION_SCHEMA = yup.object().shape({
     .trim()
     .required(t("validation.required", { field: t("form.title") }))
     .min(5, t("validation.min", { field: t("form.title"), length: 5 }))
-    .max(150, t("validation.max", { field: t("form.title"), length: 150 })),
+    .max(125, t("validation.max", { field: t("form.title"), length: 125 })),
   description: yup
     .string()
     .trim()
     .required(t("validation.required", { field: t("form.description") }))
-    .min(20, t("validation.min", { field: t("form.description"), length: 20 })),
+    .min(20, t("validation.min", { field: t("form.description"), length: 20 }))
+    .max(
+      10000,
+      t("validation.max", { field: t("form.description"), length: 10000 })
+    ),
   categories: yup
     .array()
     .required(t("validation.categoriesRequired"))
