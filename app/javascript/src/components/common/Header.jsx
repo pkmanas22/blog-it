4936f4264a1e623ecb8fce113ca872b384f6b60d
Manas Kumar Pradhan <pkmanas22@gmail.com>
2025-07-27 08:25:48 +0530

    Fixed minor changes

--- app/javascript/src/components/common/Header.jsx
+++ app/javascript/src/components/common/Header.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 import { Typography } from "neetoui";
 
-const Header = ({ label, labelTag = null, children }) => (
+const Header = ({ label, labelTag, children }) => (
   <div className="flex w-11/12 items-center justify-between py-2">
     <div className="flex items-center gap-2">
       <Typography style="h1" weight="bold">
         {label}
       </Typography>
-      {labelTag && <>{labelTag}</>}
+      {labelTag && labelTag}
     </div>
-    {children && <>{children}</>}
+    {children && children}
   </div>
 );
 
